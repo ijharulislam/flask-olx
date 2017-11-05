7166822539276b1eb7f0cbd46c49829a601eead9
ijharulislam <ijharalkawsary@gmail.com>
2017-11-05 22:55:39 +0600

    Remove py

---
+++ remove.py
@@ -0,0 +1,10 @@
+import psycopg2
+try:
+	conn = psycopg2.connect("dbname='olx_v1' user='olx' password='plx-spider' host='localhost'")
+    print "Database Connected"
+except:
+    print "I am unable to connect to the database."
+
+cur = conn.cursor()
+cur.execute("""DELETE FROM olx a USING olx b WHERE a.id < b.id AND a.adcode = b.adcode;""")
+cur.close()
