7a65659a399b036fe3d1a415c10c865acfcc6f46
ijharulislam <ijharalkawsary@gmail.com>
2017-11-05 23:16:54 +0600

    Test remove

--- remove.py
+++ remove.py
@@ -1,6 +1,6 @@
 import psycopg2
 try:
-	conn = psycopg2.connect("dbname='olx_v1' user='olx' password='plx-spider' host='localhost'")
+    conn = psycopg2.connect("dbname='olx_v1' user='olx' password='plx-spider' host='localhost'")
     print "Database Connected"
 except:
     print "I am unable to connect to the database."
